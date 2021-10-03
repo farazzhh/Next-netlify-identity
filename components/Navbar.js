8cd695b7c80c0e6a2e0e5d3fe53f054f6e97b76c
farazzhh <havaee.faraz@gmail.com>
2021-10-03 17:48:57 +0330

    start setup

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,15 +5,16 @@ export default function Navbar() {
   return (
     <div className="container">
       <nav>
-        <Image src="/rupee.png" width={50} height={48} />
-        <h1>Gaming Vibes</h1>
+        <Image src="/volks.png" width={80} height={55} />
+        
+        <h1>VolksVibes</h1>
         <ul>
           <li><Link href="/"><a>Home</a></Link></li>
           <li><Link href="/guides"><a>Guides</a></Link></li>
         </ul>
       </nav>
       <div className="banner">
-        <Image src="/banner.png" width={966} height={276} />
+        <Image src="/volks banner.png" width="auto" height="auto" />
       </div>
     </div>
   )
